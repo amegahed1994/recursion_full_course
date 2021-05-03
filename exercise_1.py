a1e9233a3c086d07db4bf407197b140ab7c12e14
Abdel Megahed <abdel.megahed@rdccaa.com>
2021-05-02 21:49:14 -0400

    solution

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,12 @@
 """
-Exercise:
+Exercise 1:
 Given a positive integer n, create a recursive function that returns the sum of its digits.
 """
+
+def sum_of_digits(n):
+    if n < 10:
+        return n
+
+    n_str = str(n)
+    return int(n_str[0]) + sum_of_digits(int(n_str[1:]))
+
