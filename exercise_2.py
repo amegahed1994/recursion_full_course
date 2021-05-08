435ec7d2439b24ad8b8358f92b808f032a0a5314
Abdel Megahed <abdel.megahed@rdccaa.com>
2021-05-08 18:26:33 -0400

    push exercise 2 solution

---
+++ exercise_2.py
@@ -0,0 +1,23 @@
+"""
+Exercise 2:
+Give an array 'arr' and integer 'num', create a recursive function that returns the
+num of occurences 'num' in 'arr'
+"""
+
+
+def num_of_occurences(num, arr):
+    if not arr:
+        return 0
+
+    elif arr[0] == num:
+        return 1 + num_of_occurences(num, arr[1:])
+
+    else:
+        return num_of_occurences(num, arr[1:])
+
+
+if __name__ == "__main__":
+    num = 3
+    arr = [3, 1, 2, 3, 3]
+
+    print(num_of_occurences(num, arr))
